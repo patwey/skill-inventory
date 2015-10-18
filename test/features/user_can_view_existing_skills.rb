397ada9417cadf56533f3beb5d2583bc19d3d5b4
Pat Wey <patrickwey@gmail.com>
2015-10-17 20:30:17 -0600

    user story comments written into view feature test

---
+++ test/features/user_can_view_existing_skills.rb
@@ -0,0 +1,16 @@
+require_relative '../test_helper'
+
+class ViewExistingSkillsTest < FeatureTest
+  # As a user, when I navigate to the skills index, and skills exist,
+  # then I see the existing skills
+
+  # As a user, when I navigate to the skills index, and skills do not exist,
+  # then I see a prompt to create a new skill
+
+  # As a user, when I navigate to the skills index, and I click on the title
+  # link of a specific skill, then I see the title and description for that skill
+  # and an option to return to the index
+
+  # As a user, when I navigate to the skills index, then I see a title link,
+  # update button and delete button for each specific skill
+end
