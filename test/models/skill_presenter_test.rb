d470c07ed0b371e3d86b4d350d41e9782041709b
Pat Wey <patrickwey@gmail.com>
2015-10-18 14:22:52 -0600

    added skill_presenter model test, but I don't know what I want it to do :(

---
+++ test/models/skill_presenter_test.rb
@@ -0,0 +1,13 @@
+require_relative '../test_helper'
+
+class SkillPresenterTest < Minitest::Test
+  def test_it_formats_skill_titles
+    # TODO: How do I want titles formatted?
+    skip
+  end
+
+  def test_it_formats_skill_descriptions
+    # TODO: How do I want descriptions formatted?
+    skip
+  end
+end
